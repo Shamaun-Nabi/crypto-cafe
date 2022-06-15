30ab0c91da28767beca5cd683baad258d49e4245
Shamaun-Nabi <snashik950@gmail.com>
2022-06-15 11:12:53 +0600

    router 404 solved address design done

--- src/Components/Contact/BdAdress.jsx
+++ src/Components/Contact/BdAdress.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export default function BdAdress() {
   return (
-   <>
-    <div className="w-[550px] h-[230px] bg-teal-400 text-white mt-5 flex justify-center">
-      This is Bd Adress
-    </div>
-   </>
+    <>
+      <div className="flex justify-center">
+        <div className="w-[550px] h-[230px] bg-slate-800 text-white mt-5 flex justify-center items-center">
+          <address>
+            Dhaka Kalabagan <br /> Visit us at: Example.com <br /> Box 564, Dhanmondi 32
+          </address>
+        </div>
+      </div>
+    </>
   );
 }
--- src/Components/Contact/UsAdress.jsx
+++ src/Components/Contact/UsAdress.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function UsAdress() {
   return (
-    <div className="w-[550px] h-[230px] bg-teal-400 text-white mt-5 flex justify-center">
-      This is Us Adress
+    <div className="flex justify-center">
+    <div className="w-[550px] h-[230px] bg-pink-500 text-white mt-5 flex justify-center items-center">
+      <address>
+        United Kingdom <br /> Visit us at: Example.com <br /> Box 5399, LA Vegas , USA
+      </address>
     </div>
+  </div>
   );
 }
